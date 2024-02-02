597bcc52734f0ea5fd3c8921903c6528aa998438
haomail <thegeniusetter@gmail.com>
2024-02-03 00:53:29 +0700

    create template_hmte

--- create_event.py
+++ create_event.py
@@ -24,7 +24,7 @@ def main():
                     {"method": "popup", "minutes":30}
                 ]
             },
-        "location": "Toko Kopi Seduh"
+        "location": "Somewhere"
         #"conferenceData": {"createRequest": {"conferenceSolutionKey": {"type": "hangoutsMeet"},"requestId": "1st Meeting Divisi Akademik"}},
         }
     event = service.events().insert(calendarId="primary",body=event,sendNotifications=True).execute()
@@ -37,30 +37,3 @@ def main():
 
 if __name__ == "__main__":
   main()
-
-
-
-"""
-event = {
-            "summary": "[HMTE] 1st Meeting Divisi Akademik",
-            "description": "Proker setahun kedepan",
-            "colorId": "9",
-            "start": {
-                "dateTime": "2024-02-02T19:00:00+07:00",
-                "timeZone": "Asia/Jakarta"
-            },
-            "end": {
-                "dateTime": "2024-02-02T19:30:00+07:00",
-                "timeZone": "Asia/Jakarta"
-            },
-            "attendees": [{"email": email} for email in emails],
-            "reminders": {
-                "useDefault": False,
-                "overrides": [
-                    {"method": "popup", "minutes":10}
-                ]
-            },
-            "conferenceData": {"createRequest": {"conferenceSolutionKey": {"type": "hangoutsMeet"},"requestId": "1st Meeting Divisi Akademik"}},
-        }
-        event = service.events().insert(calendarId="primary",body=event,sendNotifications=True,conferenceDataVersion=1).execute()
-"""
