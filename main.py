455ba4804e044f739db00573372032da10fd111c
haomail <thegeniusetter@gmail.com>
2024-02-02 17:09:04 +0700

    change the credentials.json file, fix the main.py (google meet link) and listing emails on emails.txt

--- main.py
+++ main.py
@@ -12,40 +12,50 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json")
 
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file("credentials.json",SCOPES)
-        creds = flow.run_local_server(port=0)
-    with open("token.json", "w") as token:
-        token.write(creds.to_json())
+def main():
+    creds = None
 
-try:
-    service = build("calendar", "v3", credentials=creds)
-    event = {
-        "summary": "[HMTE] 1st Meeting Divisi Akademik",
-        "description": "Proker setahun kedepan",
-        "colorId": "9",
-        "start": {
-            "dateTime": "2024-02-02T16:30:00+07:00",
-            "timeZone": "Asia/Jakarta"
-        },
-        "end": {
-            "dateTime": "2024-02-02T19:40:00+07:00",
-            "timeZone": "Asia/Jakarta"
-        },
-        "attendees": [for email in emails.txt],
-        "reminders": {
-            "useDefault": False,
-            "overrides": [
-                {"method": "popup", "minutes":10}
-            ]
-        },
-        "conferenceData": {"createRequest": {"requestId": "1st Meeting Divisi Akademik", "conferenceSolutionKey": {"type": "hangoutsMeet"}}},
-    }
-    event = service.events().insert(calendarId="primary",body=event).execute()
-    print(f"Event created {event.get('htmlLink')}")
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json")
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json",SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
-except HttpError as error:
-    print("An error occured!")
+    try:
+        service = build("calendar", "v3", credentials=creds)
+        with open('emails.txt', 'r') as file:
+            emails = [line.strip() for line in file]
+        event = {
+            "summary": "[HMTE] 1st Meeting Divisi Akademik",
+            "description": "Proker setahun kedepan",
+            "colorId": "9",
+            "start": {
+                "dateTime": "2024-02-02T19:00:00+07:00",
+                "timeZone": "Asia/Jakarta"
+            },
+            "end": {
+                "dateTime": "2024-02-02T19:30:00+07:00",
+                "timeZone": "Asia/Jakarta"
+            },
+            "attendees": [{"email": email} for email in emails],
+            "reminders": {
+                "useDefault": False,
+                "overrides": [
+                    {"method": "popup", "minutes":10}
+                ]
+            },
+            "conferenceData": {"createRequest": {"conferenceSolutionKey": {"type": "hangoutsMeet"},"requestId": "1st Meeting Divisi Akademik"}},
+        }
+        event = service.events().insert(calendarId="primary",body=event,sendNotifications=True,conferenceDataVersion=1).execute()
+        print(f"Event created {event.get('htmlLink')}")
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
+if __name__ == "__main__":
+  main()
