00bd44177f354697e97d7eb04de2ee51f2127adf
Mayori Permitha <141924190+haomail@users.noreply.github.com>
2024-02-06 13:04:09 +0700

    ganti tanggal rapat kedua akademik

--- create_event.py
+++ create_event.py
@@ -6,8 +6,43 @@ def main():
     with open('emails.txt', 'r') as file:
         emails = [line.strip() for line in file]
     event = {
+            "summary": "[HMTE] 2nd Meeting Divisi Akademik",
+            "description": "Agenda: Timeline Proker",
+            "colorId": "9",
+            "start": {
+                "dateTime": "2024-02-06T20:00:00+07:00",
+                "timeZone": "Asia/Jakarta"
+            },
+            "end": {
+                "dateTime": "2024-02-06T21:00:00+07:00",
+                "timeZone": "Asia/Jakarta"
+            },
+            "attendees": [{"email": email} for email in emails],
+            "reminders": {
+                "useDefault": False,
+                "overrides": [
+                    {"method": "popup", "minutes":10}
+                ]
+            },
+            #"location": "Toko Kopi Seduh",
+            "conferenceData": {"createRequest": {"conferenceSolutionKey": {"type": "hangoutsMeet"},"requestId": "2nd Meeting Divisi Akademik"}},
+        }
+    event = service.events().insert(calendarId="primary",body=event,sendNotifications=True,conferenceDataVersion=1).execute()
+
+    print(f"Event created at {event.get('htmlLink')}")
+    print("Created event")
+    print("id: ", event['id'])
+    print("summary: ", event['summary'])
+    print("starts at: ", event['start']['dateTime'])
+    print("ends at: ", event['end']['dateTime'])
+
+if __name__ == "__main__":
+  main()
+
+"""
+event = {
         "summary": "Create Event Using Google Calendar API",
-        "description": """Broadcast Messages""",
+        "description": "Broadcast Messages",
         "colorId": "9",
         "start": {
             "dateTime": "2024-02-03T10:00:00+07:00",
@@ -27,13 +62,5 @@ def main():
         "location": "Somewhere"
         #"conferenceData": {"createRequest": {"conferenceSolutionKey": {"type": "hangoutsMeet"},"requestId": "1st Meeting Divisi Akademik"}},
         }
-    event = service.events().insert(calendarId="primary",body=event,sendNotifications=True).execute()
-    print(f"Event created at {event.get('htmlLink')}")
-    print("Created event")
-    print("id: ", event['id'])
-    print("summary: ", event['summary'])
-    print("starts at: ", event['start']['dateTime'])
-    print("ends at: ", event['end']['dateTime'])
-
-if __name__ == "__main__":
-  main()
+    event = service.events().insert(calendarId="primary",body=event,sendNotifications=True).execute() */
+    """
